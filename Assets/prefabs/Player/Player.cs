d1d8d30c0790079e98e5f115d9cce25cbf4e14ab
Kyumi <uberuso77@gmail.com>
2021-09-22 20:53:58 -0500

    Able to move player with no gravity

--- Assets/prefabs/Player/Player.cs
+++ Assets/prefabs/Player/Player.cs
@@ -5,8 +5,12 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float walkingSpeed = 5f;
     PlayerInputActions inputActions;
     Vector2 MoveInput;
+    Vector3 Velocity;
+    CharacterController characterController;
+
     private void Awake()
     {
         inputActions = new PlayerInputActions();
@@ -24,6 +28,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        characterController = GetComponent<CharacterController>(); // give character controller component
         inputActions.Gameplay.Move1.performed += MoveInputUpdated;
         inputActions.Gameplay.Move1.canceled += MoveInputUpdated;
     }
@@ -36,6 +41,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log($"Player input is : {MoveInput}");
+        //Debug.Log($"Player input is : {MoveInput}");
+        Velocity = GetPlayerDesiredMoveDirection() * walkingSpeed;
+        characterController.Move(Velocity * Time.deltaTime);
+    }
+
+    Vector3 GetPlayerDesiredMoveDirection()
+    {
+        return new Vector3(-MoveInput.y, 0f, MoveInput.x).normalized; //if normalized speed wont increase when pressing two buttons
     }
 }
