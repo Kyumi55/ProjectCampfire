3e9e429d995e8ad95cf27d8596bf0610156a6f0f
Kyumi <uberuso77@gmail.com>
2021-09-22 20:53:17 -0500

    Able to move player with no gravity

--- Assets/prefabs/Player/Player.cs
+++ Assets/prefabs/Player/Player.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    PlayerInputActions inputActions;
+    Vector2 MoveInput;
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+    }
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inputActions.Gameplay.Move1.performed += MoveInputUpdated;
+        inputActions.Gameplay.Move1.canceled += MoveInputUpdated;
+    }
+
+    void MoveInputUpdated(InputAction.CallbackContext ctx)
+    {
+        MoveInput = ctx.ReadValue<Vector2>(); //tell us the player input
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log($"Player input is : {MoveInput}");
     }
 }
